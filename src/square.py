82270ab185f52305b76c5c6faa2290f37543780b
v.golikov <v.golikov@rdp.ru>
2024-03-14 10:47:26 +0300

    fix square class

--- src/square.py
+++ src/square.py
@@ -4,7 +4,7 @@ from rectangle import Rectangle
 class Square(Rectangle):
 
     def __init__(self, side_a: int):
-        super().__init__(side_a, side_a)
         if side_a <= 0:
             raise ValueError('side must be higher than 0')
+        super().__init__(side_a, side_a)
         self.name = 'Square'
