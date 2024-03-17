b1c83d77e8e61d9036a1cbb196574809d61139b0
v.golikov <v.golikov@rdp.ru>
2024-03-17 03:01:52 +0300

    pre pull commit

---
+++ tests/conftest.py
@@ -0,0 +1,114 @@
+import pytest
+from src.rectangle import Rectangle
+from src.triangle import Triangle
+from src.square import Square
+from src.circle import Circle
+from src.figure import Figure
+
+
+@pytest.fixture(params=[(1, 2, 2), (1.1, 1.2, 1.32)], ids=['int', 'float'])
+def rectangle_data(request):
+    """fixture for practise"""
+    side_a, side_b, area = request.param
+    yield side_a, side_b, area
+
+
+@pytest.fixture()
+def data_wrapper_areas():
+    """fixture for areas of figures"""
+
+    def _wrapper(data: str):
+        if data == 'rectangle_integer':
+            return 3, 5, 15
+        if data == 'rectangle_float':
+            return 3.5, 5.5, 19.25
+        if data == 'integer_triangle':
+            return 13, 14, 15, 84
+        if data == 'float_triangle':
+            return 11.5, 12.5, 13.5, 66.78659460363285
+        if data == 'integer_square':
+            return 5, 25
+        if data == 'float_square':
+            return 3.5, 12.25
+        if data == 'integer_circle':
+            return 5, 78.53981633974483
+        if data == 'float_circle':
+            return 5.5, 95.03317777109125
+        if data == 'square+triangle':
+            s = Square(10)
+            t = Triangle(13,14,15)
+            add_area_sum = 184
+            return s, t, add_area_sum
+
+    yield _wrapper
+
+
+@pytest.fixture()
+def data_wrapper_perimeters():
+    """fixtures for perimeters of figures"""
+
+    def _wrapper(data: str):
+        if data == 'rectangle_integer':
+            return 3, 5, 16
+        if data == 'rectangle_float':
+            return 3.5, 5.5, 18
+        if data == 'integer_triangle':
+            return 13, 14, 15, 42
+        if data == 'float_triangle':
+            return 11.5, 12.5, 13.5, 37.5
+        if data == 'integer_square':
+            return 5, 20
+        if data == 'float_square':
+            return 3.5, 14
+        if data == 'integer_circle':
+            return 5, 31.41592653589793
+        if data == 'float_circle':
+            return 5.5, 34.55751918948772
+
+    yield _wrapper
+
+
+@pytest.fixture()
+def data_wrapper_error():
+    """fixture for triangle errors"""
+
+    def _wrapper(data: str):
+        if data == 'rectangle_integer':
+            return 13, -14
+        if data == 'rectangle_float':
+            return 13.5, -15.5
+        if data == 'triangle_integer':
+            return 13, -14, 15
+        if data == 'triangle_float':
+            return 13.5, -15.5, 18
+        if data == 'triangle_sides_exists':
+            return 1, 2, 111
+        if data == 'integer_square':
+            return -5
+        if data == 'float_square':
+            return -3.5
+        if data == 'integer_circle':
+            return -5
+        if data == 'float_circle':
+            return -5.5
+
+    yield _wrapper
+
+
+@pytest.fixture()
+def data_wrapper_add_areas():
+    """fixtures for add_areas"""
+    def _wrapper(data: str):
+        if data == 'square+triangle':
+            s = Square(10)
+            t = Triangle(13,14,15)
+            return s, t, 184
+
+    yield _wrapper
+
+# @pytest.fixture()
+# def areas():
+#     s = Square(5)
+#     t = Triangle(13,14,15)
+#     c = Circle(3)
+#     r = Rectangle(3, 5)
---
+++ tests/test_classes.py
@@ -0,0 +1,166 @@
+from src.rectangle import Rectangle
+from src.triangle import Triangle
+from src.square import Square
+from src.circle import Circle
+from src.figure import Figure
+from datetime import datetime
+import pytest
+
+
+@pytest.mark.skip(reason='because i want to practise with skipping')
+def test_rectangle_skip():
+    """training example"""
+    r = Rectangle(3, 5)
+    assert r.get_area() == 15, f'Area should be {3 * 5}'
+
+
+@pytest.mark.skipif(condition=datetime.now().year == 2023, reason='practise with skip if')
+def test_rectangle_skip_if():
+    """training example"""
+    r = Rectangle(3, 5)
+    assert r.get_area() == 15, f'Area should be {3 * 5}'
+
+
+@pytest.mark.homework
+def test_positive_rectangle_custom_mark():
+    """for practise with custom run marks"""
+    r = Rectangle(3, 5)
+    assert r.get_area() == 15, f'Area should be {3 * 5}'
+
+
+@pytest.mark.parametrize(('side_a', 'side_b', 'area'),
+                         [
+                             (1, 5, 5),
+                             (3.5, 6.5, 22.75)
+
+                         ],
+                         ids=['integer', 'float']
+                         )
+def test_rectangle_parametrize(side_a, side_b, area):
+    """training example"""
+    r = Rectangle(side_a, side_b)
+    assert r.get_area() == area, f'Area should be {side_a * side_b}'
+
+
+def test_rectangle_with_fixture_from_conftest(rectangle_data):
+    """training example"""
+    side_a, side_b, area = rectangle_data
+    r = Rectangle(side_a, side_b)
+    assert r.get_area() == area, f"Area should be {area}"
+
+
+@pytest.mark.parametrize(('side_a', 'side_b'),
+                         [
+                             (11, -5),
+                             (3.5, -6.5)
+                         ],
+                         ids=['integer', 'float']
+                         )
+def test_rectangle_raise_error(side_a, side_b):
+    """example how to parametrize errors"""
+    with pytest.raises(ValueError):
+        Rectangle(side_a, side_b)
+
+
+@pytest.mark.parametrize("rectangle_types", ['rectangle_integer', "rectangle_float"])
+def test_rectangle_area(data_wrapper_areas, rectangle_types):
+    """my tests start from this"""
+    side_a, side_b, area = data_wrapper_areas(data=rectangle_types)
+    r = Rectangle(side_a, side_b)
+    assert r.get_area() == area, f"Area should be {area}"
+
+
+@pytest.mark.parametrize("rectangle_types", ['rectangle_integer', "rectangle_float"])
+def test_rectangle_perimeter(data_wrapper_perimeters, rectangle_types):
+    side_a, side_b, perimeter = data_wrapper_perimeters(data=rectangle_types)
+    r = Rectangle(side_a, side_b)
+    assert r.get_perimeter() == perimeter, f"Perimeter should be {perimeter}"
+
+
+@pytest.mark.parametrize("rectangle", ['rectangle_integer', 'rectangle_float'])
+def test_rectangle_error(data_wrapper_error, rectangle):
+    side_a, side_b = data_wrapper_error(data=rectangle)
+    with pytest.raises(ValueError):
+        Rectangle(side_a, side_b)
+
+
+@pytest.mark.parametrize("triangle", ['integer_triangle', "float_triangle"])
+def test_triangle_area(data_wrapper_areas, triangle):
+    side_a, side_b, side_c, area = data_wrapper_areas(data=triangle)
+    t = Triangle(side_a, side_b, side_c)
+    assert t.get_area() == area, f"Area should be {area}"
+
+
+@pytest.mark.parametrize("triangle", ['integer_triangle', "float_triangle"])
+def test_triangle_perimeter(data_wrapper_perimeters, triangle):
+    side_a, side_b, side_c, perimeter = data_wrapper_perimeters(data=triangle)
+    t = Triangle(side_a, side_b, side_c)
+    assert t.get_perimeter() == perimeter, f"Perimeter should be {perimeter}"
+
+
+@pytest.mark.parametrize("triangle", ['triangle_integer', 'triangle_float', 'triangle_sides_exists'])
+def test_triangle_error(data_wrapper_error, triangle):
+    side_a, side_b, side_c = data_wrapper_error(data=triangle)
+    with pytest.raises(ValueError):
+        Triangle(side_a, side_b, side_c)
+
+
+@pytest.mark.parametrize("square", ['integer_square', "float_square"])
+def test_square_area(data_wrapper_areas, square):
+    side_a, area = data_wrapper_areas(data=square)
+    s = Square(side_a)
+    assert s.get_area() == area, f"Area should be {area}"
+
+
+@pytest.mark.parametrize("square", ['integer_square', "float_square"])
+def test_square_perimeter(data_wrapper_perimeters, square):
+    side_a, perimeter = data_wrapper_perimeters(data=square)
+    s = Square(side_a)
+    assert s.get_perimeter() == perimeter, f"Perimeter should be {perimeter}"
+
+
+@pytest.mark.parametrize("square", ['integer_square', 'float_square'])
+def test_square_error(data_wrapper_error, square):
+    side_a = data_wrapper_error(data=square)
+    with pytest.raises(ValueError):
+        Square(side_a)
+
+
+@pytest.mark.parametrize("circle", ['integer_circle', "float_circle"])
+def test_circle_area(data_wrapper_areas, circle):
+    radius, area = data_wrapper_areas(data=circle)
+    c = Circle(radius)
+    assert c.get_area() == area, f"Area should be {area}"
+
+
+@pytest.mark.parametrize("circle", ['integer_circle', "float_circle"])
+def test_circle_perimeter(data_wrapper_perimeters, circle):
+    radius, perimeter = data_wrapper_perimeters(data=circle)
+    c = Circle(radius)
+    assert c.get_perimeter() == perimeter, f"Area should be {perimeter}"
+
+@pytest.mark.parametrize("circle", ['integer_circle', 'float_circle'])
+def test_circle_error(data_wrapper_error, circle):
+    radius = data_wrapper_error(data=circle)
+    with pytest.raises(ValueError):
+        Circle(radius)
+
+# @pytest.mark.parametrize("figures", ['square+triangle'])
+# def test_add_area(data_wrapper_areas, figures):
+#     s, t, add_area_sum = data_wrapper_areas(data=figures)
+#     assert s.add_area(t) == add_area_sum
+
+
+
+@pytest.mark.parametrize("figures", ['square+triangle'])
+def test_add_area(data_wrapper_areas, figures):
+    figure_1, figure_2, add_area_sum = data_wrapper_areas(data=figures)
+    assert figure_1.add_area(figure_2) == add_area_sum
+
+
+
+def test_circle_errodddr():
+    s= Square(10)
+    t = Triangle(13,14,15)
+    with pytest.raises(ValueError):
+        s.add_area(5)
